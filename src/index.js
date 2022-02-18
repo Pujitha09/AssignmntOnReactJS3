4ab79a8c61d1523eb2f6e3af90c0bd81fdbad652
Pujitha09 <20b01a1222@svecw.edu.in>
2022-02-18 13:22:46 +0530

    Third one

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Text from "./Text.js";
+import "./styles.css";
+import faker from "faker";
+
+const App = () => {
+  return (
+    <div>
+      <Text tag="Name" />
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <>
+    <App />
+  </>,
+
+  document.getElementById("root")
+);
